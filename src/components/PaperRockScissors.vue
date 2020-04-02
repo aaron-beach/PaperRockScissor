33bac83dfd066875322953ec1486983a1dd955fe
aaron_beach <aaron.r.beach@gmail.com>
2020-04-02 13:30:14 -0400

    initial commit

---
+++ src/components/PaperRockScissors.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="paper_rock_scissors"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.paper_rock_scissors {
+  background-color: blueviolet;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1 class="intro-header">
+      paper. rock. scissors.
+    </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+<style lang="scss" scoped>
+.intro-header {
+  margin: 0 auto;
+  text-transform: capitalize;
+}
+</style>
